94813c0a5ab2217c2258413681ef9b79825038bb
ThuF <jordan.p.pavlov@gmail.com>
2025-04-14 11:47:36 +0300

    Small update

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,12 @@ export async function run(): Promise<void> {
     const ms: string = core.getInput('milliseconds')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.warning(`Waiting ${ms} milliseconds ...`)
 
     // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
+    core.warning(new Date().toTimeString())
     await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.warning(new Date().toTimeString())
 
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
